e606ca6715f36db7381b4a7aa33637d2e93275c6
Vinicius 'VinyLimaZ' Lima <vinylimaz@live.com>
2020-03-04 19:47:13 -0300

    Add the controller to create a image
    (cherry picked from commit eed5b369e9c527acdc7e3b222a4ecae69d62f95d)

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,15 @@
+class ImagesController < ApplicationController
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.create(image_params)
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:owner, :description, :file)
+  end
+end
