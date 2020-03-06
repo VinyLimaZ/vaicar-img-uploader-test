b9acc838c5da4827fe659aec37e43a72ea084130
Vinicius 'VinyLimaZ' Lima <vinylimaz@live.com>
2020-03-06 18:26:57 -0300

    Allow database cleaner to work on CI

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -3,6 +3,8 @@
 RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.allow_production = false
+    DatabaseCleaner.allow_remote_database_url = true
     DatabaseCleaner.clean_with(:truncation)
   end
   config.around(:each) do |example|
